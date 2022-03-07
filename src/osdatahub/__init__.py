806c932d20e55fcfb036cd89665214a7be2fbb86
danhirstos <dan.hirst@os.uk>
2022-03-07 09:46:17 +0000

    Created skeleton testing script for Downloads API (with nothing implemented)

--- src/osdatahub/__init__.py
+++ src/osdatahub/__init__.py
@@ -1,6 +1,7 @@
 from osdatahub.extent import Extent
 from osdatahub.FeaturesAPI import FeaturesAPI
 from osdatahub.PlacesAPI import PlacesAPI
+from osdatahub.MatchAndCleanseAPI import MatchAndCleanseAPI
 
 
 __version__ = "0.0.0"
