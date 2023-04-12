48f3cb9562a51bb81deaf55784ae2e2b6bf7f046
Josh Pooley <joshpooley93@gmail.com>
2023-04-12 09:52:00 +0100

    chore: bump version and update changelog

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, OS Rapid Prototyping Team"
 author = "OS Rapid Prototyping Team"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.2"
+release = "1.2.3"
 
 # -- General configuration ---------------------------------------------------
 
