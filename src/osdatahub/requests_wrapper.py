6a40288ade659e38baeee35eb7e43854b7ea5817
Josh Pooley <joshpooley93@gmail.com>
2023-04-12 08:47:40 +0100

    docs: added docstrings to requests wrapper

--- src/osdatahub/requests_wrapper.py
+++ src/osdatahub/requests_wrapper.py
@@ -2,7 +2,19 @@ import requests
 
 
 def check_length(func):
+    """
+    Decorator function that checks if the response content length is as expected.
+    """
     def wrapper(*args, **kwargs):
+        """
+        Wrapper function that performs the content length check.
+
+        Returns:
+            The response object if the content length check passes.
+
+        Raises:
+            IOError: If the content length check fails.
+        """
         response = func(*args, **kwargs)
 
         expected_length = response.headers.get('Content-Length')
@@ -24,9 +36,35 @@ def check_length(func):
 
 @check_length
 def get(*args, **kwargs):
+    """
+    Sends a GET request to the specified URL.
+
+    Args:
+        *args: Positional arguments to be passed to the requests.get function.
+        **kwargs: Keyword arguments to be passed to the requests.get function.
+
+    Returns:
+        The response object if the content length check passes.
+
+    Raises:
+        IOError: If the content length check fails.
+    """
     return requests.get(*args, **kwargs)
- 
- 
+
+
 @check_length
 def post(*args, **kwargs):
+    """
+    Sends a POST request to the specified URL.
+
+    Args:
+        *args: Positional arguments to be passed to the requests.post function.
+        **kwargs: Keyword arguments to be passed to the requests.post function.
+
+    Returns:
+        The response object if the content length check passes.
+
+    Raises:
+        IOError: If the content length check fails.
+    """
     return requests.post(*args, **kwargs)
