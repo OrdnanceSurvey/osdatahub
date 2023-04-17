c7eecfe2a39c9f7a4f6d67677962d63249f5acdc
Andrew West <81236667+aw-west-defra@users.noreply.github.com>
2023-04-17 13:45:52 +0100

    updated main init

--- src/osdatahub/__init__.py
+++ src/osdatahub/__init__.py
@@ -14,6 +14,7 @@ __version__ = "1.2.3"
 from osdatahub.extent import Extent
 from osdatahub.FeaturesAPI import FeaturesAPI
 from osdatahub.PlacesAPI import PlacesAPI
+from osdatahub.MapsAPI import AvailableLayers, OSTileProvider
 from osdatahub.NamesAPI import NamesAPI
 from osdatahub.LinkedIdentifiersAPI import LinkedIdentifiersAPI
 from osdatahub.DownloadsAPI import OpenDataDownload, DataPackageDownload
